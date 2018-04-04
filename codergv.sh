ada2906c542f78ec4142a68a0bb1242e0370c37d
Drew Lentz <drew@codergv.org>
2018-04-04 09:03:10 -0500

    Changed paths

--- codergv.sh
+++ codergv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hackrf_transfer -t /home/codergv/spectrum_painter/spectrum_painter/codergv.iqhackrf -f 915000000 -b 10000000 -s 1000000 -x 20 -a 1
+hackrf_transfer -t codergv.iqhackrf -f 915000000 -b 10000000 -s 1000000 -x 20 -a 1
