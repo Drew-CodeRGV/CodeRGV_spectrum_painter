b0f17b033f7a50dd636b59b88947e9cfdcf1913b
Drew Lentz <drew@codergv.org>
2018-04-04 08:57:56 -0500

    sample scripts

---
+++ codergv-lp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+while true ; do hackrf_transfer -f 915000000 -s 1000000 -t /home/codergv/spectrum_painter/spectrum_painter/codergv.iqhackrf -x 20 -a 1; done
---
+++ jam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hackrf_transfer -t /home/codergv/spectrum_painter/spectrum_painter/jam.iqhackrf -f 915000000 -b 2000000 -s 2000000 -x 20 -a 1
+
